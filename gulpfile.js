fefc392222e8c86a87ffbd8c5c18edc8cced9375
Daniel Wedin <danielwedin@hotmail.se>
2017-05-31 23:33:15 +0200

    Move folders and files to better location

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,45 @@ const gulp        = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass        = require('gulp-sass');
 const prettier    = require('gulp-prettier');
+const autoprefixer = require('gulp-autoprefixer');
+const util = require('gulp-util');
 
+const config = {
+  production: !!util.env.production, // --production
+};
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass', 'scripts'], function() {
+gulp.task('serve', ['sass', 'scripts', 'copy-static'], function() {
 
   browserSync.init({
-    server: "./public"
+    server: "./dist"
   });
 
-  gulp.watch("public/*.scss", ['sass']).on('change', browserSync.reload);
-  gulp.watch("public/*.js", ['scripts']).on('change', browserSync.reload);
+  gulp.watch("src/*.scss", ['sass']).on('change', browserSync.reload);
+  gulp.watch("src/*.js", ['scripts']).on('change', browserSync.reload);
   gulp.watch("public/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('scripts', () => {
-  gulp.src('public/*.js')
+  gulp.src(['src/*.js', 'public/*.js'])
   .pipe(prettier({useFlowParser: false, printWidth: 125}))
-  .pipe(gulp.dest('public/'));
+  .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('copy-static', () => {
+  gulp.src('public/*.html')
+  .pipe(gulp.dest('dist/'));
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', () => {
-  return gulp.src("public/*.scss")
+  return gulp.src("*.scss")
   .pipe(sass())
-  .pipe(gulp.dest("public/dist/"))
+  .pipe(config.production ? autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }) : util.noop())
+  .pipe(gulp.dest("dist/"))
   .pipe(browserSync.stream());
 });
 
--- src/autocomplete.js
+++ src/autocomplete.js
@@ -1,24 +1,3 @@
-other = function() {
-  var pattern = Trianglify({
-    width: window.innerWidth,
-    height: window.innerHeight,
-    x_colors: [
-      "#8e0152",
-      "#c51b7d",
-      "#de77ae",
-      "#f1b6da",
-      "#fde0ef",
-      "#f7f7f7",
-      "#e6f5d0",
-      "#b8e186",
-      "#7fbc41",
-      "#4d9221",
-      "#276419"
-    ]
-  });
-  document.querySelector(".page-wrapper").appendChild(pattern.canvas());
-}();
-
 const autocomplete = function() {
   const KEYCODE = { ENTER: 13, ESC: 27, UPARROW: 38, DOWNARROW: 40 };
 
